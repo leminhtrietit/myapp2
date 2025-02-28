65bbd4f9384d86087e52b6e02a27c1ea1e017f21
Le Minh Triet <minhtrietofficial@gmail.com>
2025-02-28 19:09:11 +0700

    update

--- resources/views/resource.blade.php
+++ resources/views/resource.blade.php
@@ -34,7 +34,29 @@
             <div id="content">
 
                 <!-- Topbar -->
-                @include('modules.header')
+                <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+		<div class="container">
+			<a class="navbar-brand" href="index.php">MT<span>solution</span></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="oi oi-menu"></span> Menu
+			</button>
+
+			<div class="collapse navbar-collapse" id="ftco-nav">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
+					<!-- <li class="nav-item"><a href="about.html" class="nav-link">About</a></li> -->
+					<!-- <li class="nav-item"><a href="index.php?controller=tools" class="nav-link">Tools</a></li> -->
+					 <li class="nav-item"><a href="{{ route('resource.index') }}" class="nav-link">Download</a></li>
+					 <!-- <li class="nav-item"><a href="login.php" class="nav-link"><img src="Assets/images/logo_mau_sv.png" alt="Sao Viet" width="30"></a></li> -->
+
+					<!--<li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+					<li class="nav-item"><a href="contact.html" class="nav-link">Contact us</a></li>
+					<li class="nav-item cta"><a href="#" class="nav-link">Free Consultation</a></li> -->
+
+				</ul>
+			</div>
+		</div>
+	</nav>
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
@@ -89,7 +111,48 @@
             </div>
             <!-- End of Main Content -->
 
-            @include('modules.footer')
+            <footer class="ftco-footer ftco-footer-2 ftco-section">
+            <div class="container">
+                <div class="row mb-5">
+                    <div class="col-md">
+                        <div class="ftco-footer-widget mb-4">
+                            <h2 class="ftco-footer-logo">MT<span>solution</span></h2>
+                            <p>
+                                All your need!
+                            </p>
+                            <ul class="ftco-footer-social list-unstyled mt-2">
+                                <li class="ftco-animate"><a href="https://x.com/tinhoctanbinh"><span class="fa fa-twitter"></span></a></li>
+                                <li class="ftco-animate"><a href="https://www.messenger.com/t/minhtriet.info"><span class="fa fa-facebook"></span></a></li>
+                                <!-- <li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li> -->
+                                <!-- <li class="ftco-animate"><a href="mailto:contact@leminhtriet.com"><span class="bi bi-envelope-fill"></span></a></li> -->
+                            
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-md">
+                        <div class="ftco-footer-widget mb-4">
+                            <h2 class="ftco-heading-2">Have a Questions?</h2>
+                            <div class="block-23 mb-3">
+                                <ul>
+                                    <li><span class="icon fa fa-map marker"></span><span class="text">Tan Binh, Ho Chi Minh</span></li>
+                                    <li><a href="tel:+84946426536"><span class="icon fa fa-phone"></span><span class="text">+84 946 426 536</span></a></li>
+                                    <li><a href="mailto:contact@leminhtriet.com"><span class="icon fa fa-paper-plane pr-4"></span><span class="text">contact@leminhtriet.com</span></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 text-center">
+
+                        <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | by <a href="https://leminhtriet.com" target="_blank">MinhTrietProduction</a>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
 
         </div>
 
