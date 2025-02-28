40af98c4cd4ab7939ecf6b09cfcf1b748e666b78
Le Minh Triet <minhtrietofficial@gmail.com>
2025-02-28 20:18:08 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
+    <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>MinhTrietEras</title>
         <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="icon" href="public/assets/images/logo.png" type="image/x-icon" />
 
-        <link rel="stylesheet" href="public/assets/css/animate.css">
-        <link rel="stylesheet" href="public/assets/css/owl.carousel.min.css">
-        <link rel="stylesheet" href="public/assets/css/owl.theme.default.min.css">
-        <link rel="stylesheet" href="public/assets/css/magnific-popup.css">
-        <link rel="stylesheet" href="public/assets/css/flaticon.css">
-        <link rel="stylesheet" href="public/assets/css/style_detail.css">
+
         <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
+        
+        <link rel="icon" href="{{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/images/logo.png') }}" type="image/x-icon" />
+        
+        <link rel="stylesheet" href="{{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/css/animate.css') }}">
+        <link rel="stylesheet" href="{{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/css/owl.carousel.min.css') }}">
+        <link rel="stylesheet" href="{{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/css/owl.theme.default.min.css') }}">
+        <link rel="stylesheet" href="{{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/css/magnific-popup.css') }}">
+        <link rel="stylesheet" href="{{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/css/flaticon.css') }}">
+
+        <link rel="stylesheet" href="{{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/css/style_detail.css') }}">
 
     </head>
     <body>
@@ -59,9 +63,9 @@
 		</div>
 	</nav>
                 <!-- End of Topbar -->
-
+                
                 <!-- Begin Page Content -->
-                <div class="hero-wrap js-fullheight" style="background-image: url('public/assets/images/bg_1.jpg');" data-stellar-background-ratio="0.5">
+                <div class="hero-wrap js-fullheight" style="background-image: url({{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/images/bg_1.jpg') }});" data-stellar-background-ratio="0.5">
                     <div class="overlay"></div>
                     <div class="container">
                         <div class="row no-gutters slider-text js-fullheight align-items-center" data-scrollax-parent="true">
@@ -149,22 +153,21 @@
 
     <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
-    <script src="public/assets/js/jquery.min.js"></script>
-    <script src="public/assets/js/jquery-migrate-3.0.1.min.js"></script>
-    <script src="public/assets/js/popper.min.js"></script>
-    <script src="public/assets/js/bootstrap.min.js"></script>
-    <script src="public/assets/js/jquery.easing.1.3.js"></script>
-    <script src="public/assets/js/jquery.waypoints.min.js"></script>
-    <script src="public/assets/js/jquery.stellar.min.js"></script>
-    <script src="public/assets/js/owl.carousel.min.js"></script>
-    <script src="public/assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="public/assets/js/jquery.animateNumber.min.js"></script>
-    <script src="public/assets/js/scrollax.min.js"></script>
+   
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/jquery.min.js') }}"></script>
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/jquery-migrate-3.0.1.min.js') }}"></script>
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/jquery.easing.1.3.js') }}"></script>
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/jquery.waypoints.min.js') }}"></script>
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/jquery.stellar.min.js') }}"></script>
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/owl.carousel.min.js') }}"></script>
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/jquery.animateNumber.min.js') }}"></script>
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/scrollax.min.js') }}"></script>    
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/google-map.js') }}"></script>
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/main.js') }}"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-    <script src="public/assets/js/google-map.js"></script>
-    <script src="public/assets/js/main.js"></script>
-
-
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/jquery.min.js') }}"></script>
+    <script src="    {{ asset((env('APP_ENV') == 'local' ? '' : 'public/') . 'assets/js/jquery.min.js') }}"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.16.0/cdn/themes/light.css" />
     <script type="module" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.16.0/cdn/shoelace-autoloader.js"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
@@ -172,6 +175,7 @@
         let table = new DataTable('#myTable');
 
     </script>
+
 
     </body>
 </html>
