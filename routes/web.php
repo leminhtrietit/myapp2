9487c8f8f4e538d3cd8066e13b9528c1f9550b1a
Le Minh Triet <minhtrietofficial@gmail.com>
2025-02-28 18:51:28 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/resource', [App\Http\Controllers\ResourceController::class, 'index'])->name('resource.index');
 Route::get('/download', [App\Http\Controllers\ResourceController::class, 'index']);
