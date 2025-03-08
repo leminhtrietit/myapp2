7eb6647cce70abebaf29f8e5c3f32ed846890dc5
Le Minh Triet <minhtrietofficial@gmail.com>
2025-03-08 09:25:33 +0700

    update favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="icon" href="assets/images/logo.png" type="image/x-icon" />
+        <link rel="icon" href="assets/images/logo_nen.png" type="image/x-icon" />
 
         <link rel="stylesheet" href="assets/css/animate.css">
         <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
